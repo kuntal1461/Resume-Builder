e8db7fd4151bafe0f5ef238642d4e30c7fda6c61
Afrid <afridpashahp7@gmail.com>
2025-09-20 19:27:00 +0530

    addind a nextjs fe folder

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,15 @@
+import React,{useEffect,useState} from 'react'
+
+function index() {
+  useEffect(()=>{
+    console.log('index')
+  },[]);
+
+
+
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
